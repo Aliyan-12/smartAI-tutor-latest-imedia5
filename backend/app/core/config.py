900fb4ea7ef941cfd0e822fe7fc1281feb52d359
root <root@srv1554637.hstgr.cloud>
2026-04-21 13:14:58 +0000

    server fix.

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -14,7 +14,7 @@ class Settings(BaseSettings):
 
     postgres_user: str = ""
     postgres_password: str = ""
-    postgres_db: str = "smartai_tutor"
+    postgres_db: str = ""
     postgres_host: str = "localhost"
     postgres_port: int = 5432
 
